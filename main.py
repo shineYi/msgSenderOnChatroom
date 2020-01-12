b4a62631858719d427207055456fc8f2f962a0b3
Shine Yi <46551004+shineYi@users.noreply.github.com>
2020-01-12 14:33:19 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,90 @@
+import os
+import pyautogui
+import pyperclip
+import platform
+import subprocess
+
+
+def run_kakao(kakao_path):
+    print('Run KakaoTalk')
+    try:
+        subprocess.run(kakao_path)
+    except Exception:
+        print(f'[ERROR] Execute Kakaotalk path: {kakao_path}')
+        raise
+
+
+def enter_chatroom(chat_idx):
+    chat_imgs = ['chat.png', 'chat_with_msg.png']
+    for chat_png in chat_imgs:
+        try:
+            click_img(chat_png)
+        except TypeError:
+            pass
+        except Exception:
+            print(f'[ERROR] Click image: {chat_png}')
+            raise
+
+    # Focus on 1st chatroom
+    pyautogui.hotkey('home')
+    print(f"Enter the {chat_idx}th chatroom")
+    for _ in range(1, chat_idx):
+        pyautogui.press('down')
+    pyautogui.press('enter')
+
+
+# TODO: Return response to sending msg (Need Cursor check)
+def send_msg(msg, cmd):
+    pyperclip.copy(msg)
+    pyautogui.hotkey(cmd, 'v')
+    pyautogui.press('enter')
+
+
+def click_img(png_name):
+    img_path = os.path.join('img', png_name)
+    location = pyautogui.locateCenterOnScreen(img_path, confidence=0.9)
+    x, y = location
+    if subprocess.call("system_profiler SPDisplaysDataType | grep 'retina'", shell=True) == 0:
+        x = x / 2
+        y = y / 2
+    pyautogui.click(x, y)
+
+
+# XXX: KakaoTalk path is set only to default install path
+def config_by_os():
+    user_os = platform.system()
+    kakao_path = ['open', '-a', 'KakaoTalk']
+    cmd = 'command'
+    if user_os == 'Windows':
+        kakao_path = ['C:\Program Files (x86)\Kakao\KakaoTalk\KakaoTalk.exe']
+        cmd = 'ctrl'
+    return kakao_path, cmd
+
+
+def talk_check():
+    chatroom_idx = 3
+    my_msg = '출근했습니다'
+    kakao_path, cmd_key = config_by_os()
+    success = False
+
+    try:
+        run_kakao(kakao_path)
+        enter_chatroom(chatroom_idx)
+        # send_msg(my_msg, cmd_key)
+        success = True
+    except Exception as e:
+        print(f'Error: {e}')
+
+    return success
+
+
+if __name__ == "__main__":
+    pyautogui.PAUSE = 0.5
+    python_path = os.path.dirname(os.path.realpath(__file__))
+    os.chdir(python_path)
+
+    talk_result = talk_check()
+    if talk_result:
+        exit(0)
+    else:
+        exit(1)
